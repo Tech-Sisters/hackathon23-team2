5aba7cb14c14fa5a82c97d1eb338504c7c6e4fee
Marika <marika.bergman@gmail.com>
2023-11-23 22:53:23 +0000

    finalize firebaseInit

--- backend-nodejs/src/firebaseInit.cjs
+++ backend-nodejs/src/firebaseInit.cjs
@@ -1,23 +1,20 @@
+// this file uses common JS and is imported into app.js
+
 const admin = require('firebase-admin');
 const fs = require('fs');
 const path = require('path');
 
-// Get the current working directory
-console.log('Current working directory:', process.cwd());
-
-// Construct the absolute path to the serviceAccountKey.json file
 const serviceAccountPath = path.resolve(__dirname, '..', 'serviceAccountKey.json');
 
+// authentication works only if this file is found
 if (fs.existsSync(serviceAccountPath)) {
   const serviceAccount = require(serviceAccountPath);
   firebase = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 } else {
-  console.log('Service account key file not found at:', serviceAccountPath);
-  // Handle authentication being disabled or troubleshoot path issues
+  console.log('Firebase serviceAccountKey file not find, no authentication');
   firebase = admin.initializeApp({
-    // Provide default configurations or leave it empty as needed
   });
 }
 
