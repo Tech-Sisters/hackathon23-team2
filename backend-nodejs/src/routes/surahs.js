4a84e1ce0c3314ef290235990c0acb1351e9ce67
annoinspace <aneesahalmask@gmail.com>
2023-11-24 12:23:30 +0000

    getting the ayat for each surah

--- backend-nodejs/src/routes/surahs.js
+++ backend-nodejs/src/routes/surahs.js
@@ -8,44 +8,43 @@ import UsersModel from "../models/User.js"
 //TODO add authentication for this route
 surahRouter.post("/initialiseSurah", async (req, res, next) => {
   try {
-    const { auth_id } = req.query;
+    const { auth_id } = req.query
     await surahController.initialiseSurah(req, res, next),
       await currentRevisionController.initialiseCurrentRevision(req, res, next)
     // sending response after both controllers have finished updates
     const updatedUser = await UsersModel.findOne({ auth_id })
-    res.status(200).send(updatedUser);
+    res.status(200).send(updatedUser)
   } catch (err) {
     next(err)
   }
-});
-
+})
 
 // get the surahTestHistory for a specific surah
 // TODO add authentication for this route
 surahRouter.get("/surahHistory", surahController.getSurahHistory)
 
-
 // update the surah revision history
 //TODO add authentication to this route
 surahRouter.put("/updateSurah", surahController.updateSurah)
 
-// update strenght of the surah and 
+// update strenght of the surah and
 // TODO add authentication for this route
-surahRouter.put('/updateSurah', async (req, res, next) => {
+surahRouter.put("/updateSurah", async (req, res, next) => {
   try {
-    const { auth_id } = req.query;
+    const { auth_id } = req.query
 
-    await surahController.updateSurah(req, res, next);
-    await currentRevisionController.updateCurrentRevision(req, res, next);
+    await surahController.updateSurah(req, res, next)
+    await currentRevisionController.updateCurrentRevision(req, res, next)
     // sending response after both controllers have finished updates
     const updatedUser = await UsersModel.findOne({ auth_id })
-    res.status(200).send(updatedUser);
+    res.status(200).send(updatedUser)
   } catch (err) {
     next(err)
   }
-});
-
+})
 
 surahRouter.get("/getJuzzamma", surahController.getJuzzamma)
+
+surahRouter.get("/ayat", surahController.getAyat)
 
 export default surahRouter
