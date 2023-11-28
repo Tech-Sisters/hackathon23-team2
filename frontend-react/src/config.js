80e36ef29896146adbfafb7e936d0e8246ba8725
rawanshisht <rawan.shisht@gmail.com>
2023-11-28 12:02:09 +0000

    fix surah history error

--- frontend-react/src/config.js
+++ frontend-react/src/config.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "http://localhost:3000"
-// export const API_ENDPOINT = "https://zany-bear-slip.cyclic.app"
+//export const API_ENDPOINT = "http://localhost:3000"
+export const API_ENDPOINT = "https://zany-bear-slip.cyclic.app";
