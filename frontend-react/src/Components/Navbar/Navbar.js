7f2330c6306129716b3cdb4c8fbc8f3dfb6f6d01
annoinspace <aneesahalmask@gmail.com>
2023-11-28 18:50:12 +0000

    logo size increase

--- frontend-react/src/Components/Navbar/Navbar.js
+++ frontend-react/src/Components/Navbar/Navbar.js
@@ -1,33 +1,30 @@
-import { ReactComponent as Logo } from "../../Assets/TazkeerLogo.svg";
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth } from "../../firebase";
-import { useDispatch } from "react-redux";
-import { removeAccessToken } from "../../Redux/Actions/userActions";
+import logoPng from "../../Assets/Tazkeer.png"
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { auth } from "../../firebase"
+import { useDispatch } from "react-redux"
+import { removeAccessToken } from "../../Redux/Actions/userActions"
 const Navbar = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const handleLogout = () => {
     // Sign out from Firebase
     auth
       .signOut()
       .then(() => {
-        dispatch(removeAccessToken());
-        window.location.href = "/log-in";
+        dispatch(removeAccessToken())
+        window.location.href = "/log-in"
       })
       .catch((error) => {
-        console.error("Logout failed:", error);
-      });
-  };
+        console.error("Logout failed:", error)
+      })
+  }
 
   return (
-    <nav
-      className="navbar navbar-expand-lg "
-      style={{ backgroundColor: "#3C2E4C" }}
-    >
+    <nav className="navbar navbar-expand-lg " style={{ backgroundColor: "#3C2E4C" }}>
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <Logo style={{ width: "125px", height: "125px" }} />
+          <img src={logoPng} alt="Logo" style={{ width: "120px", height: "120px", padding: "15px" }} />
         </a>
 
         <>
@@ -41,10 +38,7 @@ const Navbar = () => {
             aria-label="Toggle navigation"
             style={{ backgroundColor: "#FFF" }}
           >
-            <span
-              className="navbar-toggler-icon"
-              style={{ backgroundColor: "#FFF" }}
-            ></span>
+            <span className="navbar-toggler-icon" style={{ backgroundColor: "#FFF" }}></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -59,11 +53,7 @@ const Navbar = () => {
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className="text nav-link fw-bold FontSize12"
-                  style={{ color: "#FFFFFF" }}
-                  href="/profile"
-                >
+                <a className="text nav-link fw-bold FontSize12" style={{ color: "#FFFFFF" }} href="/profile">
                   My Profile
                 </a>
               </li>
@@ -80,7 +70,7 @@ const Navbar = () => {
         </>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
